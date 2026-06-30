20fc94c7dfa4a9660773de0895b495eef9558f92
VaniChervirala02 <chervirala.vani123@gmail.com>
2026-06-30 14:45:59 +0530

    Update nyc_taxi_pipeline.py

--- dags/nyc_taxi_pipeline.py
+++ dags/nyc_taxi_pipeline.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.providers.dbt.cloud.operators.dbt import DbtCloudRunJobOperator
 from datetime import datetime, timedelta
 
 default_args = {
@@ -11,32 +11,17 @@ default_args = {
 with DAG(
     dag_id='nyc_taxi_pipeline',
     default_args=default_args,
-    description='NYC Taxi ELT pipeline using dbt',
+    description='NYC Taxi ELT pipeline using dbt Cloud',
     schedule='@daily',
     start_date=datetime(2024, 1, 1),
     catchup=False,
     tags=['nyc_taxi', 'dbt']
 ) as dag:
 
-    dbt_seed = BashOperator(
-        task_id='dbt_seed',
-        bash_command='echo "dbt seed would run here"'
+    trigger_dbt_job = DbtCloudRunJobOperator(
+        task_id='trigger_dbt_build',
+        dbt_cloud_conn_id='dbt_cloud_default',
+        job_id=1082994,
+        check_interval=10,
+        timeout=300
     )
-
-    dbt_run = BashOperator(
-        task_id='dbt_run',
-        bash_command='echo "dbt run would run here"'
-    )
-
-    dbt_test = BashOperator(
-        task_id='dbt_test',
-        bash_command='echo "dbt test would run here"'
-    )
-
-    dbt_snapshot = BashOperator(
-        task_id='dbt_snapshot',
-        bash_command='echo "dbt snapshot would run here"'
-    )
-
-    # task dependencies
-    dbt_seed >> dbt_run >> dbt_test >> dbt_snapshot
