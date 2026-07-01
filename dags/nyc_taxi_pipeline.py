7ed6d0de338418d72d5eeec663fc4428f70e3acf
VaniChervirala02 <chervirala.vani123@gmail.com>
2026-07-01 23:22:52 +0530

    Update nyc_taxi_pipeline.py

--- dags/nyc_taxi_pipeline.py
+++ dags/nyc_taxi_pipeline.py
@@ -23,4 +23,32 @@ SENSOR_CMD = (
 
 TRIGGER_CMD = (
     'curl -s -X POST '
-    '-H "Authorization: Token ' + DBT_API_TOKEN
+    '-H "Authorization: Token ' + DBT_API_TOKEN + '" '
+    '-H "Content-Type: application/json" '
+    '-d \'{"cause": "Triggered by Airflow"}\' '
+    '"https://cloud.getdbt.com/api/v2/accounts/' + DBT_ACCOUNT_ID + '/jobs/' + DBT_JOB_ID + '/run/"'
+)
+
+with DAG(
+    dag_id='nyc_taxi_pipeline',
+    default_args=default_args,
+    description='NYC Taxi ELT pipeline using dbt Cloud',
+    schedule='@daily',
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+    tags=['nyc_taxi', 'dbt']
+) as dag:
+
+    wait_for_dbt_api = BashSensor(
+        task_id='wait_for_dbt_api',
+        bash_command=SENSOR_CMD,
+        poke_interval=30,
+        timeout=300
+    )
+
+    trigger_dbt_job = BashOperator(
+        task_id='trigger_dbt_build',
+        bash_command=TRIGGER_CMD
+    )
+
+    wait_for_dbt_api >> trigger_dbt_job
