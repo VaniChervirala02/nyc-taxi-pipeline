ede71fe4566cc95677027d4637e5b23f407040e7
VaniChervirala02 <chervirala.vani123@gmail.com>
2026-07-01 23:25:35 +0530

    Update nyc_taxi_pipeline.py

--- dags/nyc_taxi_pipeline.py
+++ dags/nyc_taxi_pipeline.py
@@ -14,20 +14,9 @@ DBT_ACCOUNT_ID = "271288"
 DBT_JOB_ID = "1082994"
 DBT_API_TOKEN = Variable.get("DBT_API_TOKEN")
 
-SENSOR_CMD = (
-    'curl -s -o /dev/null -w "%{http_code}" '
-    '-H "Authorization: Token ' + DBT_API_TOKEN + '" '
-    '"https://cloud.getdbt.com/api/v2/accounts/' + DBT_ACCOUNT_ID + '/" '
-    '| grep -q "200"'
-)
+SENSOR_CMD = 'curl -s -o /dev/null -w "%{http_code}" -H "Authorization: Token ' + DBT_API_TOKEN + '" "https://cloud.getdbt.com/api/v2/accounts/' + DBT_ACCOUNT_ID + '/" | grep -q "200"'
 
-TRIGGER_CMD = (
-    'curl -s -X POST '
-    '-H "Authorization: Token ' + DBT_API_TOKEN + '" '
-    '-H "Content-Type: application/json" '
-    '-d \'{"cause": "Triggered by Airflow"}\' '
-    '"https://cloud.getdbt.com/api/v2/accounts/' + DBT_ACCOUNT_ID + '/jobs/' + DBT_JOB_ID + '/run/"'
-)
+TRIGGER_CMD = 'curl -s -X POST -H "Authorization: Token ' + DBT_API_TOKEN + '" -H "Content-Type: application/json" -d \'{"cause": "Triggered by Airflow"}\' "https://cloud.getdbt.com/api/v2/accounts/' + DBT_ACCOUNT_ID + '/jobs/' + DBT_JOB_ID + '/run/"'
 
 with DAG(
     dag_id='nyc_taxi_pipeline',
